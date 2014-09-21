af78bf3df850afb4b9e951d70efd209e198e9f70
muhammedabuali <muhammedabuali@gmail.com>
2014-09-21 18:44:37 +0300

    implement g-search func

--- search.go
+++ search.go
@@ -1,23 +1,39 @@
-package AI2048
+package main
 
 type Node interface {
 	get_parent() Node
 	get_operator() int
 	get_depth() int
-	get_cost() int
+	get_path_cost() int
 	get_children() []Node
 }
 
 type Problem interface {
 	init_state() Node
-	test_success(n Node)
+	test_success(n Node) bool
 	do_action(x int) Node
-	get_cost(n Node, x int)
+	get_action_cost(n Node, x int)
 	get_actions() int //to get the number of childeren b
 }
 
 type Strategy func(nodes []Node, children []Node) []Node
 
-func general_search(p Problem, quing_fun Strategy) {
-
+func general_search(p Problem, quing_fun Strategy) Node {
+	var nodes []Node
+	var output Node
+	nodes = append(nodes, p.init_state())
+	for {
+		if len(nodes) == 0 {
+			return output
+		} else {
+			node := nodes[0]
+			nodes = nodes[1:]
+			if p.test_success(node) {
+				return node
+			} else {
+				var children []Node = node.get_children()
+				nodes = quing_fun(nodes, children)
+			}
+		}
+	}
 }
--- sec.go
+++ sec.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func hello() {
 	fmt.Println("hello")
+	var a []int
+	fmt.Println(len(a))
+	a = append(a, 4)
+	fmt.Println(len(a))
+	a = make([]int, 10)
+	fmt.Println(len(a))
+	a = a[10:]
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+	var n Node
+	fmt.Println(n == nil)
 }
