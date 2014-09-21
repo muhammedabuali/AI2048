5d2a187d34c3cda7c45876bb2204171a652ba779
muhammedabuali <muhammedabuali@gmail.com>
2014-09-21 17:44:47 +0300

    added g-search interface

---
+++ search.go
@@ -0,0 +1,23 @@
+package AI2048
+
+type Node interface {
+	get_parent() Node
+	get_operator() int
+	get_depth() int
+	get_cost() int
+	get_children() []Node
+}
+
+type Problem interface {
+	init_state() Node
+	test_success(n Node)
+	do_action(x int) Node
+	get_cost(n Node, x int)
+	get_actions() int //to get the number of childeren b
+}
+
+type Strategy func(nodes []Node, children []Node)
+
+func general_search(p Problem, quing_fun Strategy) {
+
+}
